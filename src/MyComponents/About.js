6a3fc722a4fe688e01e8a388c9f2739cf86b26b6
Gourav Rana <gouravrana870@gmail.com>
2022-01-04 13:07:48 +0530

    Intial commit

---
+++ src/MyComponents/About.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const About = () => {
+    return (
+        <div>
+            This is an about component
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iste vitae beatae rem aperiam? Sapiente delectus, consequatur culpa quas assumenda ipsam minima soluta natus necessitatibus eligendi optio tenetur in distinctio ducimus!</p>
+        </div>
+    )
+}
